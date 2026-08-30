d432208064dcbb6152e6ea2053f57772752eebe5
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-08-30 14:44:10 +0000

    Exemplo

--- 13-classe-com-metodo-estatico.js
+++
@@ -1,8 +0,0 @@
-class Retangulo {
-    static area(base, altura) {
-        let resp = parseFloat(base) * parseFloat(altura); 
-        return resp.toFixed(2);
-    }
-}
-
-console.log("Área: " + Retangulo.area(6, 7));
