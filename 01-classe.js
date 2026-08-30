94e562926604256e2a5b03253505181779d63b82
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-08-30 13:39:44 +0000

    Exemplo

--- 01-classe.js
+++ 01-classe.js
@@ -1,5 +1,5 @@
 class Retangulo {
-    //constructor(){}
+    //constructor(){} //Atualmente, JavaScript aceita apenas 1 construtor.
     constructor(baseUSR, alturaUSR) { //Atualmente, JavaScript aceita apenas 1 construtor.
         this.base = baseUSR;
         this.altura = alturaUSR;
