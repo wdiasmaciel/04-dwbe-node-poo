90b7399ef9e9c9bf3e614ea9423ab9f6c9dea249
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-08-30 14:11:11 +0000

    Exemplo

---
+++ 08-classe-com-metodo-privado.js
@@ -0,0 +1,17 @@
+class Retangulo {
+    #base; //Propriedade privada.
+    #altura; //Propriedade privada.     
+    constructor() { }
+    set setBase(base) { this.#base = base; }
+    get getBase() { return this.#base; }
+    set setAltura(altura) { this.#altura = altura; }
+    get getAltura() { return this.#altura; }
+    get #area() { return this.#base * this.#altura; } //Método privado.
+    get getArea() { return this.#area; }
+}
+
+var ret = new Retangulo();
+ret.setBase = 6; 
+ret.setAltura = 7;
+console.log("Área: " + ret.getArea);
+//console.log("Área: " + ret.#area); // -> ERRO: acesso indevido a método privado.
